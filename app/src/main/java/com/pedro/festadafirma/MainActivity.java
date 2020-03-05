a02bd63cb6262f36ab2a227ef5a898f29035c157
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-03-05 14:03:23 -0300

    Feita navegação entre activitys

--- app/src/main/java/com/pedro/festadafirma/MainActivity.java
+++ app/src/main/java/com/pedro/festadafirma/MainActivity.java
@@ -2,13 +2,41 @@ package com.pedro.festadafirma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = findViewById(R.id.text_today);
+        this.mViewHolder.textDaysLeft = findViewById(R.id.text_days_left);
+        this.mViewHolder.buttonConfirm = findViewById(R.id.button_confirm);
+
+        this.mViewHolder.buttonConfirm.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_confirm){
+         Intent intent = new Intent(this, DetailsActivity.class);
+         startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder{
+        TextView textToday;
+        TextView textDaysLeft;
+        Button buttonConfirm;
+
     }
 }
