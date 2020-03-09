bbf7d26ce3116411080c3c974469b7f4c4addb19
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-03-09 14:26:25 -0300

    Aplicando OnResume - MainActivities

--- app/src/main/java/com/pedro/festadafirma/view/DetailsActivity.java
+++ app/src/main/java/com/pedro/festadafirma/view/DetailsActivity.java
@@ -3,21 +3,42 @@ package com.pedro.festadafirma.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 
 import com.pedro.festadafirma.R;
+import com.pedro.festadafirma.constants.FestaDaFirmaConstants;
+import com.pedro.festadafirma.data.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+        this.mSecurityPreferences = new SecurityPreferences(this);
 
+        this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.check_participate){
+            if(this.mViewHolder.checkParticipate.isChecked()){
+                //salvar presença
+                this.mSecurityPreferences.storeString(FestaDaFirmaConstants.PRESENCE_KEY, FestaDaFirmaConstants.CONFIRMATION_YES);
+
+            } else {
+                // salvar ausência
+                this.mSecurityPreferences.storeString(FestaDaFirmaConstants.PRESENCE_KEY, FestaDaFirmaConstants.CONFIRMATION_NO);
+            }
+        }
     }
 
     private static class ViewHolder{
--- app/src/main/java/com/pedro/festadafirma/view/MainActivity.java
+++ app/src/main/java/com/pedro/festadafirma/view/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.pedro.festadafirma.R;
+import com.pedro.festadafirma.constants.FestaDaFirmaConstants;
+import com.pedro.festadafirma.data.SecurityPreferences;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -17,11 +19,15 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     private ViewHolder mViewHolder = new ViewHolder();
     private static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+    private SecurityPreferences mSecurityPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mSecurityPreferences = new SecurityPreferences(this);
 
         this.mViewHolder.textToday = findViewById(R.id.text_today);
         this.mViewHolder.textDaysLeft = findViewById(R.id.text_days_left);
@@ -37,10 +43,27 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        this.verifyPresence();
+    }
+
+    @Override
     public void onClick(View v) {
         if(v.getId() == R.id.button_confirm){
          Intent intent = new Intent(this, DetailsActivity.class);
          startActivity(intent);
+        }
+    }
+
+    private void verifyPresence(){
+        String presence = this.mSecurityPreferences.getStoredString(FestaDaFirmaConstants.PRESENCE_KEY);
+        if (presence.equals("")){
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.nao_confirmado));
+        } else if(presence.equals(FestaDaFirmaConstants.CONFIRMATION_YES)){
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.sim));
+        }else{
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.nao));
         }
     }
 
